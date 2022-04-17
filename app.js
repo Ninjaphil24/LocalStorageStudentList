58c3d0df6e79ab85da2014772545210a37eb0d96
Ninjaphil24 <mphilippos@hotmail.com>
2022-04-17 10:49:20 +0300

    Start of tutorial

---
+++ app.js
@@ -0,0 +1,5 @@
+const studentForm = document.getElementById("studentForm");
+const studentsContainer = document.querySelector(".students");
+const nameInput = studentForm["name"]; //This is a way to catch the child element of a parent element.  Check index.html to see the relation.
+const ageInput = studentForm["age"];
+const rollInput = studentForm["roll"];
